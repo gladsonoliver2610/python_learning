574fbd191e84bbab997ca9da462d4e1f2ab282ec
gladsonoliver2610 <sgoliver2610@gmail.com>
2026-07-26 23:27:15 +0530

    3rd worksheet (string,list)

--- 3.Stringss/worksheet.py
+++ 3.Stringss/worksheet.py
@@ -86,3 +86,75 @@
 #     print("yes it is the reverse of another")
 # else:
 #     print("no it is not the reverse of another")
+
+# 3.b
+# 1
+# string_fruit=["apple","banana","orange"]
+# print("string:",string_fruit)
+# integer=[10,20,30]
+# print("integer:",integer)
+# mixed=[10,"apple",3.14,]
+# print("mixed:",mixed)
+# nexted=[[1,2],[3,4],[5,6]]
+# print("nexted:",nexted)
+# 2
+# a=[[1,2],
+#    [3,4]]
+# b=[[5,6],
+#    [7,8]]
+# c=[[0,0],[0,0]]
+# for i in range(2):
+#     for j in range(2):
+#         for k in range(2):
+#             c[i][j] += a[i][k] * b[k][j]
+# print("answer:")
+# for row in c:
+#     print(row)
+# 4
+# numbers=[]
+# n=int(input("enter number of element:"))
+# for i in range(n):
+#     num=int(input("enter number:"))
+#     numbers.append(num)
+# print("list:", numbers)
+# print("maximum number:", max(numbers))
+# 5
+# student1=[80,75,70]
+# student2=[75,70,80]
+# student3=[70,80,75]
+# first=[student1[0],student2[0],student3[0]]
+# print("marks of the first subjects:",first)
+# 6
+# number=[12,45,7,89,23]
+# largest=number[0]
+# for num in number:
+#     if num > largest:
+#         largest=num
+# print("largest numbers:", largest)
+# 7
+# numbers=[10,15,22,33,44,51]
+# even=0
+# odd=0
+# for num in numbers:
+#     if num % 2==0:
+#         even+=1
+#     else:
+#         odd+=1
+# print("even numbers:", even)
+# print("odd numbers:", odd)
+# 8
+# marks=[80,75,90,85,70]
+# total=0
+# for mark in marks:
+#     total+=mark
+# average=total/len(marks)
+# print("sum:", total)
+# print("average:", average)
+# 9
+# numbers=[2,4,2,5,2,7,8]
+# search=4
+# count=0
+# for num in numbers:
+#     if num == search:
+#          count+=1
+# print(search, "append", count, "time")

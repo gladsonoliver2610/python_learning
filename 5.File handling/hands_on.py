91d2baed799e4c349ccacbab3cd59dc1c90b43dc
gladsonoliver2610 <sgoliver2610@gmail.com>
2026-09-02 20:28:14 +0530

    5.File Handling

--- 5.File handling/hands_on.py
+++ 5.File handling/hands_on.py
@@ -15,12 +15,19 @@
 # f=open("gladdy_2.txt","w")
 # f.write("hello gladson")
 # f.close()
-with open("gladdy_2.txt","r") as file:
-    content=file.readlines(150)
-    print(content)
+# with open("gladdy_2.txt","r") as file:
+#     content=file.readlines(150)
+#     print(content)
     # for i in content:
     #     words=i.split()
     #     for j in words:
     #         if j=="you":
     #             print("you is present in the file")
     #             break
+import pickle
+data={"name":"gladson","age":20,"city":"Chennai"}
+with open("data.pkl","wb") as f:
+    pickle.dump(data,f)
+with open("data.pkl","rb") as f:
+    loaded_data=pickle.load(f)
+    print(loaded_data)
---
+++ 5.File handling/worksheet.py
@@ -0,0 +1,25 @@
+#1
+# f=open("student.txt","x")
+# f.write("Name: Rahul\nAge:21\nCousrse:Python")
+# f.close()
+#2
+# f=open("gladdy_3.txt","r")
+# print(f.read())
+# f.close()
+#3
+# f=open("cities.txt","x")
+# f.write("Cities:\n Chennai\n Mumbai\n Calcutta")
+# f.close()
+#4
+# f=open("classwork.txt","r")
+# content=f.read()
+# for i in content[::3]:
+#     print(i)
+# f.close()
+#5
+# f=open("classwork.txt","r")
+# content=f.read()
+# f.close()
+# f=open("copy.txt","w")
+# f.write(content)
+# f.close()
